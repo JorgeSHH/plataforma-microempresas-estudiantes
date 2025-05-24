528f22425288a20c88eecf39949fb290874143b9
Jorge <jorgehh1000@gmail.com>
2025-05-24 02:42:59 -0300

    fin de la tercera parte del registro

--- registros/registroEstudiantes.php
+++ registros/registroEstudiantes.php
@@ -144,82 +144,184 @@
     <article class="parte3">
         <div class="caja-muestra">
             <div class="caja-con-scroll">
-                <div class="contenido-largo">
-                <div class="barra-progreso">
-            Soy la barra de progreso
-        </div>
-        <h2 class="tituloDos">Creacion de la cuentas</h2>
-        <div class="containerFormulario2">
-            <div class="caja-inputs">
-                <h3>Datos personales</h3>
-                <label for="telf" class="label-basico">
-                    Nombre
-                    <input type="text" name="telf" id="telf" class="input-basico">
-                </label>
-                <label for="rif" class="label-basico">
-                    Apellido
-                    <input type="text" name="rif" id="rif" class="input-basico">
-                </label>
-                <label for="rif" class="label-basico">
-                    Cedula de identidad
-                    <input type="text" name="rif" id="rif" class="input-basico">
-                </label>
-            </div>
-            <div class="caja-inputs">
-              <h3>  </h3>
-                <label for="sector" class="label-basico">
-                    Telefono
-                    <input type="text" name="sector" id="sector" class="input-basico">
-                </label>
-                <label for="sector" class="label-basico">
-                    Fecha de nacimiento
-                    <input type="date"  name="sector" id="sector" class="input-basico">
-                </label>
-                <label for="calle" class="label-basico">
-                    Sexo
-                    <div class="dos-datos-radio">
-                    <label for="estado">
-                        Masculino
-                        <input type="radio" name="genero" id="estado" class="input-basico-dos">
-                    </label>
-                    <label for="parroquia">
-                        Femenino
-                        <input type="radio" name="genero" id="parroquia" class="input-basico-dos">
-                    </label>
+                <div class="contenido-form3">
+                    <div class="barra-progreso">
+                        Soy la barra de progreso
                     </div>
-                </label>
+                    <h2 class="tituloDos">Creacion de la cuentas</h2>
+                    <p class="textoAdition">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure nesciunt dolorem odio!
+                    </p>
+                    <div class="containerFormulario3">
+                        <div class="caja-inputs">
+                            <h3>Informacion personal</h3>
+                            <br>
+                            <label for="telf" class="label-basico">
+                                Nivel de educacion
+                                <input type="text" name="telf" id="telf" class="input-basico">
+                            </label>
+                            <br>
+                            <label for="requisitos" class="label-basico">
+                                Resumen profesional
+                                 <textarea name="requisitos" id="requisitos" cols="5" rows="5"></textarea>
+                            </label>
+                        </div>
+                        <hr>
+                        <div class="caja-inputs">
+                            <div class="caja-lateral">
+                                <label for="sector" class="label-basico">
+                                    Telefono
+                                    <input type="text" name="sector" id="sector" class="input-basico">
+                                </label>
+                                <label for="sector" class="label-basico">
+                                    Fecha de nacimiento
+                                    <input type="text"  name="sector" id="sector" class="input-basico">
+                                </label>
+                            </div>
+                            <div class="boxAdition">
+                                <div class="habilidadesTecnicas">
+                                    <label for="requisitos" class="label-basico">
+                                        Resumen profesional
+                                        <textarea name="requisitos" id="requisitos" cols="5" rows="8"></textarea>
+                                    </label>
+                                </div>
+                                <div class="preferenciaTrabajo">
+                                    <label for="requisitos" class="label-basico">
+                                        Resumen profesional
+                                        <p class="textoAdition">
+                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure nesciunt dolorem odio!
+                                        </p>
+                                        <button type="button" class="btn-basico">elegir</button>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <!-- registros individuales manejar lo del registro individual con atencion -->
+                        <div class="caja-inputs">
+                            <h4>Cursos realizados</h4>
+                            <div class="caja-generadora">
+                                <div class="marco-inputs">
+                                    <label for="sector" class="label-basico">
+                                        Telefono
+                                        <input type="text" name="sector" id="sector" class="input-basico">
+                                    </label>
+                                    <label for="sector" class="label-basico">
+                                        Telefono
+                                        <input type="text" name="sector" id="sector" class="input-basico">
+                                    </label>
+                                    <label for="sector" class="label-basico">
+                                        Telefono
+                                        <input type="text" name="sector" id="sector" class="input-basico">
+                                    </label>
+                                </div>
+                                        <div class="dos-datos-radio">
+                                            <button type="button" class="btn-basico-tres">cancelar</button>
+                                            <button type="button" class="btn-basico-dos">enviar</button>
+                                         </div>
+                            </div>
+                            <br>
+                            <div class="tabla-contenedor">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Telefono</th>
+                                            <th>Telefono</th>
+                                            <th>Telefono</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>María González</td>
+                                            <td>maria@ejemplo.com</td>
+                                            <td >Editar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Carlos Pérez</td>
+                                            <td>carlos@ejemplo.com</td>
+                                            <td>Editar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ana López</td>
+                                            <td>ana@ejemplo.com</td>
+                                            <td>Editar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Juan Martínez</td>
+                                            <td>juan@ejemplo.com</td>
+                                            <td>Editar</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                        <div class="caja-inputs">
+                            <h4>Cursos realizados</h4>
+                            <div class="caja-generadora">
+                                <div class="marco-inputs">
+                                    <label for="sector" class="label-basico">
+                                        Telefono
+                                        <input type="text" name="sector" id="sector" class="input-basico">
+                                    </label>
+                                    <label for="sector" class="label-basico">
+                                        Telefono
+                                        <input type="text" name="sector" id="sector" class="input-basico">
+                                    </label>
+                                    <label for="sector" class="label-basico">
+                                        Telefono
+                                        <input type="text" name="sector" id="sector" class="input-basico">
+                                    </label>
+                                </div>
+                                        <div class="dos-datos-radio">
+                                            <button type="button" class="btn-basico-tres">cancelar</button>
+                                            <button type="button" class="btn-basico-dos">enviar</button>
+                                         </div>
+                            </div>
+                            <br>
+                            <div class="tabla-contenedor">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Telefono</th>
+                                            <th>Telefono</th>
+                                            <th>Telefono</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>María González</td>
+                                            <td>maria@ejemplo.com</td>
+                                            <td >Editar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Carlos Pérez</td>
+                                            <td>carlos@ejemplo.com</td>
+                                            <td>Editar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ana López</td>
+                                            <td>ana@ejemplo.com</td>
+                                            <td>Editar</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Juan Martínez</td>
+                                            <td>juan@ejemplo.com</td>
+                                            <td>Editar</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
             
-            <div class="caja-inputs">
-            <h3>Direccion de la habitación</h3>
-                <div class="dos-datos">
-                    <label for="estado" class="label-basico" >
-                        Estado
-                        <input type="text" name="estado" id="estado" class="input-basico-dos">
-                    </label>
-                    <label for="parroquia" class="label-basico">
-                        Parroquia
-                        <input type="text" name="parroquia" id="parroquia" class="input-basico-dos">
-                    </label>
-                </div>
-                <label for="sector" class="label-basico">
-                    Sector
-                    <input type="text" name="sector" id="sector" class="input-basico">
-                </label>
-                <label for="calle" class="label-basico">
-                    Calle
-                    <input type="text" name="calle" id="calle" class="input-basico">
-                </label>
-            </div>
-
-        </div>
-       
-                </div>
-            </div>
         </div>
         <div class="dos-datos-radio">
             <a href="/plataforma-microempresas-estudiantes/registros/registroEstudiantes.php#ancla2-1" class="btn-basico">volver</a>
-           <button class="btn-basico">Enviar</button>
+            <button class="btn-basico">Enviar</button>
         </div>
     </article>
     <p id="ancla3">.</p>
