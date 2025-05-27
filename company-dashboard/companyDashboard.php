5236ecb5855258f64fa21f8819f50b16f5dc8aa2
Fallutax <axelarmandoultimate@gmail.com>
2025-05-26 20:04:45 -0400

    Puse para que pueda ir al perfil de la empresa desde el dashboard de la compania

--- company-dashboard/companyDashboard.php
+++ company-dashboard/companyDashboard.php
@@ -37,7 +37,7 @@
                     </a>
                 </li>
                 <li class="menu-item">
-                    <div class="menu-link" ondblclick="window.location.href='../student-profile/studentProfile.php';">
+                    <div class="menu-link" ondblclick="window.location.href='../company-profile/companyProfile.php';">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                         </svg>
